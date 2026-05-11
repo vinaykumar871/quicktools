8ca2cf91922039d2186ffdf79310f52aee27dad1
vinaykumar871 <vinaypathipati8@gmail.com>
2026-05-11 12:38:59 +0530

    fresh clean upload

---
+++ public/blog-data.js
@@ -0,0 +1,10 @@
+let blogs = JSON.parse(localStorage.getItem("blogs")) || [
+
+{
+  id: 1,
+  title: "Top 50 Aptitude Questions for Placements",
+  description: "Practice important aptitude questions",
+  content: "<h2>Aptitude</h2><p>Sample content...</p>"
+}
+
+];
